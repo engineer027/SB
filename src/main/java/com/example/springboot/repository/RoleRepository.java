10d4a3b7351ecb2b8fa953f660578b06f524a6aa
engineer027 <dp270597dma@gmail.com>
2021-03-01 15:08:40 +0200

    fix checkstyle

--- src/main/java/com/example/springboot/repository/RoleRepository.java
+++ src/main/java/com/example/springboot/repository/RoleRepository.java
@@ -1,9 +1,8 @@
 package com.example.springboot.repository;
 
 import com.example.springboot.model.Role;
-import java.util.Optional;
-
 import com.example.springboot.model.RoleType;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
